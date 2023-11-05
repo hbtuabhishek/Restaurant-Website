e067d8b459a9db19dd72c951d3399f722ed2a318
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-11-05 14:13:46 +0530

    adding-a-model-via-react-portal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import classes from "./Cart.module.css";
+import Modal from "../UI/Modal";
 
 const Cart = (props) => {
   const cartItems = (
@@ -11,17 +12,18 @@ const Cart = (props) => {
     </ul>
   );
   return (
-    <div>
+    <Modal>
       {cartItems}
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>35.62</span>
-      </div>ss
+      </div>
       <div className={classes.actions}>
         <button className={classes['button--alt']}>Close</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 export default Cart;
+
