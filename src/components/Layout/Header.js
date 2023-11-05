2f6068bdd5c91449eb760216a3b31477577a9ce2
hbtuabhishek <hbtu.abhishek1@gmail.com>
2023-11-05 16:13:15 +0530

    managing-cart-and-modal-state

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
       <header className={classes.header}>
         <h1>Reactals</h1>
 
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart}/>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
